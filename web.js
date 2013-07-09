789cd93f3fc5bca63f818e29d3e7244135367e9e
woongek <woongek@yahoo.com>
2013-07-09 15:54:00 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var msg = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!' + msg.toString('utf8', 0, msg.length) ;
 });
 
 var port = process.env.PORT || 5000;
