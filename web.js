38f39633ed8a0289ff205f72f7976060219704df
woongek <woongek@yahoo.com>
2013-07-09 15:59:43 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var msg = fs.readFileSync('index.html')
+var msg = fs.readFileSync('/bitstarter/index.html')
 
 app.get('/', function(request, response) {
   response.send('Hello World! 2') ;
