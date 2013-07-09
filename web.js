e7081e6a1fae079e55a7661ed1cb36b4631f0aad
woongek <woongek@yahoo.com>
2013-07-09 15:10:19 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer= new Buffer(fs.readFileSync('~/bitstarter/index.html'), "utf-8")
 
 app.get('/', function(request, response) {
-  response.send("what a world");
+  response.send('what a world');
 });
 
 var port = process.env.PORT || 5000;
